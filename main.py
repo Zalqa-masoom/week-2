44d47efd6a4ed5d19c4bf3f1eefd9a150388c9b6
Zalqa <zalqamasoom02@gmail.com>
2026-07-22 12:24:56 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-import os
-from fastapi import FastAPI
-from dotenv import load_dotenv
-load_dotenv()
+from fastapi import FastAPI, Depends
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
 app = FastAPI()
+DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+
+# Tables create karna
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def read_root():
-    app_title = os.getenv("APP_NAME", "Simple App")
-    return {
-        "message": "Hello from FastAPI!",
-        "app_name": app_title
-    }
+    return {"message": "FastAPI and Database setup is working!"}
+@app.post("/items/")
+def create_item(name: str, description: str, db: Session = Depends(get_db)):
+    db_item = Item(name=name, description=description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return {"status": "success", "item": {"id": db_item.id, "name": db_item.name, "description": db_item.description}}
